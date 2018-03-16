d48d28d9403a2f45e532d5cd047a17febd5a7ae8
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-03-16 09:17:23 -0400

    a

--- Lab 4.1 Cards/src/DeckTester.java
+++ Lab 4.1 Cards/src/DeckTester.java
@@ -7,7 +7,13 @@ public class DeckTester
 	
 	public static void main(String args[])
 	{
+		String[] rank= {"1", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King"};
+		String[] suit= {"Heart", "Spade", "Club", "Diamond"};
+		int[] pointValue= { 1,2,3,4,5,6,7,8,9,10,11,12,13 };
 		
+	}
 		
+	
+		Deck test1 = new Deck(rank, suit, value);
 	}
 }
