7460d90f0fea2182821f44181049896e1068b448
Lingli <Lingli@Anesthesia>
2018-03-18 13:51:35 -0400

    s

--- Lab 4.1 Cards/src/DeckTester.java
+++ Lab 4.1 Cards/src/DeckTester.java
@@ -12,10 +12,13 @@ public class DeckTester
 		int[] pointValue= { 1,2,3,4,5,6,7,8,9,10,11,12,13 };
 		
 	
-		Deck test1 = new Deck(rank,suit,pointValue);
-		test1.shuffle();
-		test1.deal();
-		
+		Deck unDealt = new Deck(rank,suit,pointValue);
+		unDealt.shuffle();
+
+		while (unDealt.size() != 0)
+		{
+			System.out.println(unDealt.deal());
+		}
 		
 	}
 }
